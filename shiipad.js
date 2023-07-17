6ec2a5305bdcfbdf4ed354d7156306cb438b5145
Shii <97753399+shii0004@users.noreply.github.com>
2023-07-17 10:22:57 +0530

    Update shiipad.js

--- shiipad.js
+++ shiipad.js
@@ -1,6 +1,9 @@
 const save = () => {
   const link = document.createElement.("a");
   const content = document.querySelector.("textarea").value;
-  const file = new Blob ([content], { 'text/plain' });
-  link.href = 
+  const file = new Blob([content], { type; 'text/plain' });
+  link.href = URL.createObjectURL(file);
+  link.download = "untitled.txt";
+  link.click();
+  URL.revokeObjectURL(link.href);
 }
